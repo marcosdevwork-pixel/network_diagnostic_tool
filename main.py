bf5d39a6503078eca9bf7cd915ef3b099ed0cbd2
Marcos <marcosdevwork@gmail.com>
2026-09-04 12:34:43 -0300

    hostname ip search

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("s")
+# imports
+import socket
+
+# Socket creation
+socket.socket()
+
+# user input 
+host = input("Enter a hostname: ")
+
+# hostname ip search
+ip = socket.gethostbyname(host)
+
+# program output
+print(f"Hostname: {host} | IP:{ip}")
