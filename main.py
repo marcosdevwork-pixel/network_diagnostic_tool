52b175c10725066dd4991fec8cee23f881af62bd
Marcos <marcosdevwork@gmail.com>
2026-09-10 11:39:52 -0300

    add error identification

--- main.py
+++ main.py
@@ -12,20 +12,46 @@ port = 443
 # User input 
 host = input("Enter a hostname: ")
 
-# Network diagnosis
+dns_success = False
+tcp_status = "Not tested"
+tcp_error = ""
+ip = "Not resolved"
+connection_time = 0
+ping_status = "Not tested"
+packet_sent = 0
+packets_received = 0
+packet_lost = "N/A"
+minimum = "N/A"
+maximum = "N/A"
+average = "N/A"
 try:
     # DNS resolution
     ip = socket.gethostbyname(host)
+    dns_success = True
 
-    # TCP connection test
-    start = time.perf_counter()
+except socket.gaierror as error:
+    print(error)
+    tcp_status = "not tested"
+    tcp_error = "DNS resolution failed"
 
-    s.connect((ip, port))
+if dns_success == True:
+    try:
+        # TCP connection test
+        start = time.perf_counter()
 
-    end = time.perf_counter()
+        s.connect((ip, port))
 
-    connection_time = (end - start) * 1000
+        end = time.perf_counter()
 
+        connection_time = (end - start) * 1000
+        tcp_status = "Successful"
+
+    except ConnectionRefusedError as error:
+        tcp_status = "Failed"
+        tcp_error = "Connection refused"
+
+
+try:
     # Ping test
     ping_results = subprocess.run(
             ["ping", host],
@@ -50,11 +76,23 @@ try:
             minimum = parts[2].replace(",", "")
             maximum = parts[5].replace(",", "")
             average  = parts[8]
-     
-# Diagnostic report
-    output = f"""
+
+        if int(packets_received) > 0:
+            ping_status = "Successful"
+        else:
+            ping_status = "Not tested"
+
+           
+
+except subprocess.SubprocessError as error:
+    ping_error = str(error)
+
+
+
+ # Diagnostic report
+output = f"""
 =======================================
-          NETWORK DIAGNOSTIC
+        NETWORK DIAGNOSTIC
 =======================================
 
 TARGET:
@@ -62,12 +100,12 @@ TARGET:
     IP address: {ip}
 
 TCP connection
-    Status: Successful
+    Status: {tcp_status}
     Port: {port}
     Connection time: {connection_time:.2f} ms
 
 PING 
-    Status: Successful
+    Status: {ping_status}
     Packets sent: {packet_sent}
     Packets received: {packets_received}
     Packet loss: {packet_lost}
@@ -76,10 +114,11 @@ PING
     Maximum latency: {maximum}
 
     """
-    print(output)
+print(output)
 
-except:
-    print("Connection failed")
+
+
+
     
 
 
